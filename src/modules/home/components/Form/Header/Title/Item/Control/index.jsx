cb3ebde9dfaf66cecbd0bc9aab3f62e857945937
Phucdh <phucdh410@gmail.com>
2022-12-01 18:36:53 +0700

    Fix[STUDENT]: fix chức năng chấm điểm lại theo interface mới

--- src/modules/home/components/Form/Header/Title/Item/Control/index.jsx
+++ src/modules/home/components/Form/Header/Title/Item/Control/index.jsx
@@ -1,96 +1,93 @@
-import React, { memo, useState } from 'react';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import React, { memo, useMemo, useContext } from 'react';
 
-import { Checkbox, Grid } from '@mui/material';
+import { StudentMarksContext } from '_modules/home/components/Form';
 
-import { actions } from '_slices/mark.slice';
+import TypeInput from './TypeInput';
+import TypeCheckbox from './TypeCheckbox';
+import TypeSelect from './TypeSelect';
 
-import { CInput } from '_controls/';
-
-const Control = memo(({ id, min, max, initialMark, control, currentMark }) => {
+const Control = memo(({ id, min, max, mark, control, category, unit, options }) => {
 	//#region Data
-	const { role_id } = useSelector((state) => state.auth.profile, shallowEqual);
 
-	const [mark, setMark] = useState(initialMark);
+	// const [mark, setMark] = useState(initialMark);
 
-	const dispatch = useDispatch();
+	const { itemsMark } = useContext(StudentMarksContext);
+
+	const currentMark = useMemo(() => {
+		if (!itemsMark?.length)
+			return {
+				personal_mark_level: 0,
+				class_mark_level: 0,
+				department_mark_level: 0,
+			};
+
+		const foundItem = itemsMark.find((e) => e.item.id?.toString() === id?.toString());
+
+		if (!foundItem)
+			return {
+				personal_mark_level: 0,
+				class_mark_level: 0,
+				department_mark_level: 0,
+			};
+
+		return {
+			personal_mark_level: foundItem.personal_mark_level,
+			class_mark_level: foundItem.class_mark_level,
+			department_mark_level: foundItem.department_mark_level,
+		};
+	}, [id, itemsMark]);
+
+	const initialMark = useMemo(() => {
+		return currentMark.personal_mark_level;
+	}, [currentMark.personal_mark_level]);
+
+	const renderControl = useMemo(() => {
+		switch (control) {
+			case 0:
+				return (
+					<TypeInput
+						id={id}
+						min={min}
+						max={max}
+						mark={mark}
+						unit={unit}
+						category={category}
+						initialMark={initialMark}
+						currentMark={currentMark}
+					/>
+				);
+			case 1:
+				return (
+					<TypeCheckbox
+						id={id}
+						mark={mark}
+						unit={unit}
+						initialMark={initialMark}
+						currentMark={currentMark}
+					/>
+				);
+			case 2:
+				return (
+					<TypeSelect
+						id={id}
+						unit={unit}
+						options={options}
+						initialMark={initialMark}
+						currentMark={currentMark}
+					/>
+				);
+			default:
+				break;
+		}
+	}, [control]);
 	//#endregion
 
-	//#region Event
-	const onCheck = (item_id, mark) => (e) => {
-		const markObj = {
-			item_id: Number(item_id),
-			personal_mark_level: e.target.checked ? mark : 0,
-		};
+	// //#region Event
 
-		setMark(e.target.checked ? mark : 0);
-		dispatch(actions.updateMarks(markObj));
-	};
-
-	const onChange = (item_id, min, max) => (e) => {
-		let value = Number(e.target.value);
-		if (isNaN(value)) value = 0;
-		if (value > max) value = max;
-		if (value < min) value = min;
-
-		const markObj = {
-			item_id: Number(item_id),
-			personal_mark_level: value,
-		};
-
-		setMark(value);
-		dispatch(actions.updateMarks(markObj));
-	};
-	//#endregion
+	// //#endregion
 
 	//#region Render
-	return control === 1 ? (
-		<>
-			<Grid item xs={1.2} textAlign='center'>
-				<Checkbox disabled={role_id !== 0} onChange={onCheck(id, min)} checked={!!mark} />
-			</Grid>
-			<Grid item xs={1.2} textAlign='center'>
-				<Checkbox disabled={role_id !== 1} checked={currentMark.class_mark_level > 0} />
-			</Grid>
-			<Grid item xs={1.2} textAlign='center'>
-				<Checkbox
-					disabled={role_id !== 2}
-					checked={currentMark.department_mark_level > 0}
-				/>
-			</Grid>
-		</>
-	) : (
-		<>
-			<Grid item xs={1.2} textAlign='center'>
-				<CInput
-					disabled={role_id !== 0}
-					fullWidth
-					type='number'
-					inputProps={{ min, max }}
-					onChange={onChange(id, min, max)}
-					value={mark}
-				/>
-			</Grid>
-			<Grid item xs={1.2} textAlign='center'>
-				<CInput
-					disabled={role_id !== 1}
-					fullWidth
-					type='number'
-					inputProps={{ min, max }}
-					value={currentMark.class_mark_level}
-				/>
-			</Grid>
-			<Grid item xs={1.2} textAlign='center'>
-				<CInput
-					disabled={role_id !== 2}
-					fullWidth
-					type='number'
-					inputProps={{ min, max }}
-					value={currentMark.department_mark_level}
-				/>
-			</Grid>
-		</>
-	);
+	return <>{renderControl}</>;
 
 	//#endregion
 });
--- src/modules/home/components/Form/Header/Title/Item/index.jsx
+++ src/modules/home/components/Form/Header/Title/Item/index.jsx
@@ -1,42 +1,12 @@
-import React, { memo, useMemo, useContext } from 'react';
+import React, { memo } from 'react';
 
 import { Grid, Typography } from '@mui/material';
-
-import { StudentMarksContext } from '../../..';
 
 import Control from './Control';
 
 const Item = memo(({ data }) => {
 	//#region Data
-	const { itemsMark } = useContext(StudentMarksContext);
 
-	const currentMark = useMemo(() => {
-		if (!itemsMark?.length)
-			return {
-				personal_mark_level: 0,
-				class_mark_level: 0,
-				department_mark_level: 0,
-			};
-
-		const foundItem = itemsMark.find((e) => e.item.id?.toString() === data.id?.toString());
-
-		if (!foundItem)
-			return {
-				personal_mark_level: 0,
-				class_mark_level: 0,
-				department_mark_level: 0,
-			};
-
-		return {
-			personal_mark_level: foundItem.personal_mark_level,
-			class_mark_level: foundItem.class_mark_level,
-			department_mark_level: foundItem.department_mark_level,
-		};
-	}, [data?.id, itemsMark]);
-
-	const initialMark = useMemo(() => {
-		return currentMark.personal_mark_level;
-	}, [currentMark.personal_mark_level]);
 	//#endregion
 
 	//#region Event
@@ -50,28 +20,15 @@ const Item = memo(({ data }) => {
 				<Typography>- {data.content}</Typography>
 			</Grid>
 
-			<Grid item xs={2} textAlign='center'>
-				{!data.category ? (
-					<Typography component='span' whiteSpace='nowrap' fontWeight={500}>
-						&nbsp;&#40;{`${data.from_mark}${data?.unit ? ` ${data.unit}` : ''}`}
-						&#41;&nbsp;
-					</Typography>
-				) : (
-					<Typography component='span' whiteSpace='nowrap' fontWeight={500}>
-						&nbsp;&#40;
-						{`Từ ${data.from_mark} đến ${data.to_mark} ${data.unit ? data.unit : ''}`}
-						&#41;&nbsp;
-					</Typography>
-				)}
-			</Grid>
-
 			<Control
 				id={data.id}
 				min={data.from_mark}
 				max={data.to_mark}
+				mark={data.mark}
 				control={data.control}
-				initialMark={initialMark}
-				currentMark={currentMark}
+				category={data.category}
+				unit={data.unit}
+				options={data.options || []}
 			/>
 		</>
 	);
